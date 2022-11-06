887ba77329299729a5cd2e0bc19012e7bda71676
IvanHromenko <ivangromenko.1003@gmail.com>
2022-11-06 19:01:01 +0200

    simple home-cart navigation

--- src/components/Pages/NavBar.tsx
+++ src/components/Pages/NavBar.tsx
@@ -1,10 +1,17 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import ShoppingBasketOutlinedIcon from '@mui/icons-material/ShoppingBasketOutlined';
 import React from "react";
+import {Link} from 'react-router-dom';
 
 const NavBar = () => {
 
     return (
-        <Box>NavBar</Box>
+        <Box>
+            <Link to='/'>Online Shop</Link>
+            <Link to='/cart'><ShoppingBasketIcon/></Link><Typography>0</Typography>
+
+        </Box>
     );
 };
 
