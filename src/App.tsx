488f6b083e968f06a93dd7b1476f42586f86f090
IvanHromenko <ivangromenko.1003@gmail.com>
2022-11-06 18:10:38 +0200

    Added routes to Home page and cart

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
+
+import { Box } from '@mui/material';
 import React from 'react';
-import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Cart from './components/Pages/Cart';
+import HomePage from './components/Pages/HomePage';
+import NavBar from './components/Pages/NavBar';
 
 const App = () => {
   return (
-    <div className="App">
-    </div>
+    <Box>
+      <BrowserRouter>
+        <NavBar/>
+        <Routes>
+          <Route path='/' element={<HomePage/>}/>
+          <Route path='/cart' element={<Cart/>}/>
+        </Routes>
+      </BrowserRouter>
+    </Box>
   );
 }
 
---
+++ src/components/Pages/Cart.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+const Cart = () => {
+
+    return (
+        <Box>Cart</Box>
+    );
+};
+
+export default Cart;
