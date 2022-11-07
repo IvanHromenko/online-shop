f8137972398b28790f3e8f7a62218aa4c1614af2
IvanHromenko <ivangromenko.1003@gmail.com>
2022-11-07 06:54:51 +0200

    Added products fetching & hook

--- src/components/redux/productSlice.ts
+++ src/components/redux/productSlice.ts
@@ -1,12 +1,25 @@
-import {createSlice} from '@reduxjs/toolkit';
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
 
 import { Product } from '../Types/products';
+
+export const productFetch = createAsyncThunk(
+    "products/productFetch",
+    async () => {
+        return await (await fetch('https://api.escuelajs.co/api/v1/products'))?.json()
+    }
+);
 
 const initialState: Product[] = [];
 const productSlice = createSlice({
     name: "products",
     initialState,
-    reducers: {}
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(productFetch.fulfilled, (state, action) => {
+            return action.payload;
+        })
+
+    }
 });
 
 const productReducer = productSlice.reducer;
