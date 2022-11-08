63f908c6d7b5e76b86b4f4af8f5260025b68e0be
IvanHromenko <ivangromenko.1003@gmail.com>
2022-11-08 06:34:42 +0200

    display products on the main page

--- src/components/Pages/HomePage.tsx
+++ src/components/Pages/HomePage.tsx
@@ -1,12 +1,31 @@
+import { Grid } from "@mui/material";
 import { Box } from "@mui/system";
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+
+import { useAppDispatch, useAppSelector } from "../redux/hook/reduxHooks";
+import { productFetch } from "../redux/productSlice";
 
 const HomePage = () => {
 
+    const products = useAppSelector(state => state.productReducer);
+    const dispatch = useAppDispatch();
+    useEffect(() => {
+        dispatch(productFetch())
+    }, [dispatch])
+
     return (
         <Box>
-            Home page
+           <Grid container spacing={3}>
+                {
+                    products.map((item) => (
+                        <Grid item xs={4} key={item.id}>
+                            <img src={`${item.images}`}/>
+                            <h4>{`${item.title}`}</h4>
+                        </Grid>
+                    ))
+                }
+           </Grid>
         </Box>
     );
 };
