bb017ff6d0442a144098ee7f5f17ac3d1c53cd45
IvanHromenko <ivangromenko.1003@gmail.com>
2022-11-16 19:24:19 +0200

    remove from cart

--- src/components/Pages/Cart.tsx
+++ src/components/Pages/Cart.tsx
@@ -1,12 +1,32 @@
-import { Box } from "@mui/material";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Box, Button } from "@mui/material";
 import { NavLink } from "react-router-dom";
-import { useAppSelector } from "../redux/hook/reduxHooks";
+
+import { useAppDispatch, useAppSelector } from "../redux/hook/reduxHooks";
 import { RootState } from "../redux/store";
+import { CartItem } from "../Types/cart";
+import { deleteFromCart } from "../redux/cart";
 
 
 const Cart = () => {
     const cart = useAppSelector((state: RootState) => state.cartReducer);
+    const dispatch = useAppDispatch();
+
+    const subTotal = () => {
+        cart.reduce((total, item) => total + item.product.price * item.quantity, 0);
+    };
+
+    const [total, setTotal] = useState(subTotal());
+
+    useEffect(() => {
+        subTotal();
+        setTotal(subTotal());
+    }, [cart]);
+
+    const deleteItemFromCart = (item: CartItem) => {
+        dispatch(deleteFromCart(item.product.id));
+    }
+    
     return (
         <Box>
             {cart.length === 0 ? (
@@ -16,9 +36,13 @@ const Cart = () => {
                 </Box>
             ) : (
                 cart.map((item) => (
-                    <Box>
+                    <Box key={item.product.id}>
                         <img src={`${item.product.images}`} alt="cartprod-img"></img>
                         <h3>{item.product.title}</h3>
+                        <Button onClick={() => {
+                            deleteItemFromCart(item);
+                            setTotal();
+                        }}>Remove</Button>
                     </Box>
                 ))
             )}
